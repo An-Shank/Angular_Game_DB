3a5afcbd3f9ae47af3611e8e31e38f42dfce2fee
An-Shank <caanandshankar.mec@gmail.com>
2021-12-13 13:54:27 -0600

    Added Game Details
     - Added page to display details of the games
     - Displays details, screenshots and trailers

--- angular-game-db/src/app/app-routing.module.ts
+++ angular-game-db/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetailsComponent } from './components/details/details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -10,6 +11,10 @@ const routes: Routes = [
   {
     path: 'search/:game-search',
     component: HomeComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
   }
 ];
 
